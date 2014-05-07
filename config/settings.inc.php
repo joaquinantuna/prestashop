a877cc47c72900d4e5291dcfe436e0b4e2913fcd
joaquinantuna <joaquinantuna@gmail.com>
2014-05-06 21:23:35 -0300

    otra

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', '1CEdTbPGvUWy5tKC9NwxEazGG1OxNXrtYgHSag3MXV1X23TOiEZgmuqd');
+define('_COOKIE_IV_', '30kxc4eG');
+define('_PS_CREATION_DATE_', '2014-02-04');
+define('_PS_VERSION_', '1.5.6.2');
+define('_RIJNDAEL_KEY_', 'j4Uhs9ipJ4x4QkGBmz0AmNdziaAV97Vd');
+define('_RIJNDAEL_IV_', 'FSSs1C81Ah7DgIekHGxFkA==');
